b091df67a82e0692379809fd2f3a0f3bf26a41e3
otokarsefer <otokar.sefer@voco.ee>
2024-09-16 11:44:07 +0300

    fuck

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req, res) => {
+    const tasks = ["Study HTML", "Study CSS", "Study JS", "Study OOP"] 
     res.render("index")
 })
 
